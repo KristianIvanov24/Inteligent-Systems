736771b45c1b8a939bfe0031b98806a218e9d991
Kristian Ivanov <105101729+KristianIvanov24@users.noreply.github.com>
2023-11-06 00:41:23 +0200

    Update main2.cpp

--- homeworks/HW1/main2.cpp
+++ homeworks/HW1/main2.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
-
-//Globals
+#include <cstdlib> // Include for std::abs
+#include <climits> // Include for INT_MAX
 
 const int FOUND = -24;
 
@@ -14,101 +14,80 @@ int goalIdxOfZero;
 
 std::vector<std::string> path;
 
-namespace moves{
-    bool up(){
-        if(currIdxOfZero / sideSize < 1)
+namespace moves {
+    bool up() {
+        if (currIdxOfZero / sideSize < 1)
             return false;
 
-        std::swap(tiles[currIdxOfZero], tiles[currIdxOfZero+sideSize]);
+        std::swap(tiles[currIdxOfZero], tiles[currIdxOfZero + sideSize]);
         currIdxOfZero += sideSize;
         return true;
     }
 
-    bool down(){
-        if(currIdxOfZero / sideSize > sideSize - 1)
+    bool down() {
+        if (currIdxOfZero / sideSize >= sideSize - 1)
             return false;
 
-        std::swap(tiles[currIdxOfZero], tiles[currIdxOfZero-sideSize]);
+        std::swap(tiles[currIdxOfZero], tiles[currIdxOfZero - sideSize]);
         currIdxOfZero -= sideSize;
         return true;
     }
 
-    bool left(){
-        if(currIdxOfZero % sideSize == sideSize - 1)
+    bool left() {
+        if (currIdxOfZero % sideSize == sideSize - 1)
             return false;
 
-        std::swap(tiles[currIdxOfZero], tiles[currIdxOfZero+1]);
+        std::swap(tiles[currIdxOfZero], tiles[currIdxOfZero + 1]);
         currIdxOfZero += 1;
         return true;
     }
 
-    bool right(){
-        if(currIdxOfZero % sideSize == 0)
+    bool right() {
+        if (currIdxOfZero % sideSize == 0)
             return false;
 
-        std::swap(tiles[currIdxOfZero], tiles[currIdxOfZero-1]);
+        std::swap(tiles[currIdxOfZero], tiles[currIdxOfZero - 1]);
         currIdxOfZero -= 1;
         return true;
     }
 }
 
-/*void printBoard(){
-    for(int i = 0; i <= boardSize; i++){
-        if(i % sideSize == 0)
-            std::cout << std::endl;
-        std::cout << titles[i] << ' ';
-    }
-    std::cout << std::endl;
-}*/
-
-int manhattan()
-{
+int manhattan() {
     int sum = 0;
     int positionsBeforeZeroLeft = goalIdxOfZero;
     int cur;
     int x;
-    for (int i = 0; i < sideSize; i++)
-    {
-        for (int j = 0; j < sideSize; j++)
-        {
+    for (int i = 0; i < sideSize; i++) {
+        for (int j = 0; j < sideSize; j++) {
             x = j + i * sideSize;
             cur = tiles[x];
-            if (cur == 0)
-            {
+            if (cur == 0) {
                 continue;
             }
 
             if (positionsBeforeZeroLeft > 0) {
-                sum += abs((cur - 1) / sideSize - i) + abs((cur - 1) % sideSize - j);
+                sum += std::abs((cur - 1) / sideSize - i) + std::abs((cur - 1) % sideSize - j);
                 --positionsBeforeZeroLeft;
-            }
-            else {
-                sum += abs(cur / sideSize - i) + abs(cur % sideSize - j);
+            } else {
+                sum += std::abs(cur / sideSize - i) + std::abs(cur % sideSize - j);
             }
         }
     }
     return sum;
 }
 
-// is this board the goal board?
-bool isGoal()
-{
+bool isGoal() {
     return manhattan() == 0;
 }
 
-int search(int g, int threshold)
-{
+int search(int g, int threshold) {
     int f = g + manhattan();
 
-    //greater f encountered
-    if (f > threshold)
-    {
+    if (f > threshold) {
         return f;
     }
 
-    //Goal node found
-    if (isGoal())
-    {
+    if (isGoal()) {
         std::cout << path.size() - 1 << std::endl;
         for (int i = 1; i < path.size(); i++) {
             std::cout << path[i] << std::endl;
@@ -119,68 +98,55 @@ int search(int g, int threshold)
     int min = INT_MAX;
     int temp;
 
-    if (path.back() != "up" && moves::down())
-    {
+    if (path.empty() || path.back() != "down" && moves::down()) {
         path.push_back("down");
         temp = search(g + 1, threshold);
-        if (temp == FOUND)
-        {
+        if (temp == FOUND) {
             return FOUND;
         }
-        if (temp < min)
-        {
+        if (temp < min) {
             min = temp;
         }
         path.pop_back();
         moves::up();
     }
-    if (path.back() != "down" && moves::up())
-    {
+    if (path.empty() || path.back() != "up" && moves::up()) {
         path.push_back("up");
         temp = search(g + 1, threshold);
-        if (temp == FOUND)
-        {
+        if (temp == FOUND) {
             return FOUND;
         }
-        if (temp < min)
-        {
+        if (temp < min) {
             min = temp;
         }
         path.pop_back();
         moves::down();
     }
-    if (path.back() != "left" && moves::right())
-    {
+    if (path.empty() || path.back() != "right" && moves::right()) {
         path.push_back("right");
         temp = search(g + 1, threshold);
-        if (temp == FOUND)
-        {
+        if (temp == FOUND) {
             return FOUND;
         }
-        if (temp < min)
-        {
+        if (temp < min) {
             min = temp;
         }
         path.pop_back();
         moves::left();
     }
-    if (path.back() != "right" && moves::left())
-    {
+    if (path.empty() || path.back() != "left" && moves::left()) {
         path.push_back("left");
         temp = search(g + 1, threshold);
-        if (temp == FOUND)
-        {
+        if (temp == FOUND) {
             return FOUND;
         }
-        if (temp < min)
-        {
+        if (temp < min) {
             min = temp;
         }
         path.pop_back();
         moves::right();
     }
 
-    //return the minimum 'f' encountered greater than threshold
     return min;
 }
 
@@ -189,11 +155,9 @@ bool idastar() {
     path.push_back("init");
     int temp;
 
-    while (true)
-    {
+    while (true) {
         temp = search(0, threshold);
-        if (temp == FOUND)
-        {
+        if (temp == FOUND) {
             return true;
         }
         if (temp > threshold) {
@@ -214,27 +178,27 @@ void solve() {
     }
 }
 
-void input(){
+void input() {
     std::cin >> boardSize;
     std::cin >> goalIdxOfZero;
 
     int temp;
-    for(int i = 0; i <= boardSize; i++){
+    for (int i = 0; i < boardSize; i++) {
         std::cin >> temp;
 
-        if(temp == 0)
+        if (temp == 0)
             currIdxOfZero = i;
 
         tiles.push_back(temp);
     }
 
-    if(goalIdxOfZero == -1)
+    if (goalIdxOfZero == -1)
         goalIdxOfZero = boardSize;
 
-    sideSize = sqrt(boardSize+1);
+    sideSize = std::sqrt(boardSize + 1);
 }
 
-int main(){
+int main() {
     input();
     solve();
 }
